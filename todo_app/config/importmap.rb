7f0069bcd94d388abf00376a29d1f06d40201480
takeru0076 <takeru200276@icloud.com>
2024-04-17 11:30:56 +0900

    bootstrapのinstall

--- todo_app/config/importmap.rb
+++ todo_app/config/importmap.rb
@@ -1,3 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
+
+pin "bootstrap", to: "bootstrap.min.js", preload: true #バージョンによっては、"bootstap.min.js"ではなく、"bootsrap.js"と書く
+pin "@popperjs/core", to: "popper.js", preload: true
